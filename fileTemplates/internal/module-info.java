6437c1b4fcfe42b222110a9c50fd60a6a4565e68
hanyong <hanyong10@126.com>
2022-12-19 10:11:06 +0800

    IU-2021.3.3 <purui@DESKTOP-L8BMASL Create baseRefactoring.xml, IntelliLang.xml, ide.general.xml, mybatisx.xml, git.xml, project.default.xml, advancedSettings.xml, diff.xml, Enum.java, Interface.java, yiiguxing.translation.xml, shared-indexes.xml, terminal.xml, Default.xml, CodeGlancePro.xml, find.xml, ignored-suggested-plugins.xml, one_dark_config.xml, ui.lnf.xml, module-info.java, filetypes.xml, keymap.xml, package-info.java, colors.scheme.xml, databaseDrivers.xml, maven.wrapper.mapping.xml, templates.xml, AnnotationType.java, javarebel.xml, Record.java, laf.xml, GrepConsole.xml, jpb-application-settings.xml, Default.xml, nodejs.xml, Windows.xml, Class.java, jdk.table.xml, vcs.xml, toStringTemplates.xml, editor.xml, certificates.xml, debugger.xml, MyGroup.xml, updates.xml

---
+++ fileTemplates/internal/module-info.java
@@ -0,0 +1,8 @@
+#parse("File Header.java")
+ /**
+  * TODO
+  * @author: han
+  * @date: ${YEAR}-${MONTH}-${DAY} ${TIME}
+  */
+module #[[$MODULE_NAME$]]# {
+}
---
+++ fileTemplates/internal/package-info.java
@@ -0,0 +1,7 @@
+#parse("File Header.java")
+ /**
+  * TODO
+  * @author: han
+  * @date: ${YEAR}-${MONTH}-${DAY} ${TIME}
+  */
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
